c866a44c0bd3736aacc695c2ba504f574a1ba8af
Jude M Battista <jbattista20@my.whitworth.edu>
2018-05-14 12:36:25 -0700

    Fix stupid, stupid typo

--- audioCodec.m
+++ audioCodec.m
@@ -1,18 +1,17 @@
-
-
-function [out] = audioCodec ()
+function [M, x0, y0, out] = baseAudioCodec ()
   n = 32;             % length of window
   nb = 127;           % number of windows, > 1
   b = 4; L = 5;       % Quantization information
   q = 2*L/(2^b - 1);  % b bits on the interval [-L, L]
   for i = 1:n
     for j = 1:2*n
-      M(i, j) = cos((i-1 + 1/2) + (j-1 + 1/2 + n/2) * pi/n);
+      M(i, j) = cos((i-1 + 1/2) * (j-1 + 1/2 + n/2) * pi/n);
     end
   end
   M = sqrt(2/n) * M;
   N = M';             % Inverse MDCT
-  Fs = 8172; f = 7;   % Fs = sampling rate
+  Fs = 8172;            % Fs = sampling rate
+  f = 7;   
   x = cos((1:4096)*pi*64*f/4096);   %test signal
   sound(x, Fs);       % Matlab's sound command_line_path
   out = [];
@@ -31,3 +30,4 @@ function [out] = audioCodec ()
 pause(1);
 sound(out, Fs);  
 end
+
